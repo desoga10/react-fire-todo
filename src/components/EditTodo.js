f9a3505121ea99f79ce114b6fea7e51f52935b2d
desoga10 <done2stay@gmail.com>
2022-12-21 01:27:57 +0100

    Initial commit

---
+++ src/components/EditTodo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditTodo = () => {
+  return (
+    <div>EditTodo</div>
+  )
+}
+
+export default EditTodo
---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <h2>Todo</h2>
+  )
+}
+
+export default Todo
