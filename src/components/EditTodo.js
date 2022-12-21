d41d95e506e46f124fdd0a5966f82bc5a2dffb6e
desoga10 <done2stay@gmail.com>
2022-12-22 00:43:56 +0100

    Implemented page design

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -2,7 +2,49 @@ import React from 'react'
 
 const EditTodo = () => {
   return (
-    <div>EditTodo</div>
+    <>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Edit Todo
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="editLabel"
+        aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="editLabel">Update Todo Details</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+
+              <form className="d-flex">
+                <input
+                  type="text"
+                  className="form-control"
+                />
+              </form>
+
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button
+                type="button"
+                className="btn btn-primary"
+              >Update Todo</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,77 @@
 import React from 'react'
+import EditTodo from './EditTodo'
 
 const Todo = () => {
   return (
-    <h2>Todo</h2>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-white">
+              <div className="card-body">
+                <button
+                  data-bs-toggle="modal"
+                  data-bs-target="#addModal"
+                  type="button"
+                  className="btn btn-info">Add Todo
+                </button>
+
+
+                <div className="todo-list">
+                  <div className="todo-item">
+                    <hr />
+                    <span>
+                      <div className="checker" >
+                        <span className="" >
+                          <input
+                            type="checkbox"
+                          />
+                        </span>
+                      </div>
+                      &nbsp; Go hard or Go Home<br />
+                      <i>10/11/2022</i>
+                    </span>
+                    <span className=" float-end mx-3">
+                      <EditTodo /></span>
+                    <button
+                      type="button"
+                      className="btn btn-danger float-end"
+
+                    >Delete</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal */}
+      <div className="modal fade" id="addModal" tabIndex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <form className="d-flex">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="addModalLabel">Add Todo</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Add a Todo"
+                />
+              </div>
+              <div className="modal-footer">
+                <button className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                <button className="btn btn-primary">Create Todo</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
